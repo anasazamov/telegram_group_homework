41e870c449a2d3d134e0975db2970f4adaaf297d
anasazamov <anasazamov55@gmail.com>
2023-02-04 21:32:27 +0500

    replay

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -9,17 +9,12 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    arr = []
-    for i in data['messages']:
-        if i['type'] == 'service':
-            
-                arr.append(i['actor'])
-        elif i['type'] == 'message':
-            
-                arr.append(i['from'])
+    ls=[]
+    for i in data["messages"]:
+        if "from" in i.keys():
+            ls.append(i["from"])
+    return list(set(ls))
     
-    return list( set( arr ))
-
 path_file="data/result.json"
 data=read_data(path_file)
 print(find_all_users_name(data))
