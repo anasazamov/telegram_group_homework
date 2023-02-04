01647a8d68e9ed609ca7fbf4056493c7fe48c3f2
anasazamov <anasazamov55@gmail.com>
2023-02-04 21:53:54 +0500

    home

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -10,11 +10,12 @@ def find_all_users_name(data: dict)->list:
         list: List containing all the users name.
     """
     ls=[]
-    for i in data["messages"]:
-        if "from" in i.keys():
-            ls.append(i["from"])
-        elif "actor" in i.keys():
-            ls.append(i["actor"])
+    for i in data["messages"]:   
+            if not i.get("from_id","abc").startswith("channel"):
+                if "from" in i.keys():
+                    ls.append(i["from"])
+                elif "actor" in i.keys():
+                    ls.append(i["actor"])
     return list(set(ls))
     
 path_file="data/result.json"
